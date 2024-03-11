57bcb5cb99ee84f239e1c03d194014c797627a3c
kushal-02 <saikushalyennam870@gmail.com>
2024-03-11 10:51:35 +0530

    edited to gui-based

--- script.js
+++ script.js
@@ -1,45 +1,111 @@
-function getComputerChoice(){
+// computer randomly select one from rock, or paper, or scissors.
+
+let answer = '';
+let computerScore = 0;
+let playerScore = 0;
+
+
+// HTML elements
+
+let resultText = document.querySelector('.resultText');
+let playerTotal = document.querySelector('.playerTotal');
+let computerTotal = document.querySelector('.computerTotal');
+
+function reset(){
+    computerScore = 0;
+    playerScore = 0;
+    answer = 'Please click rock, paper, or scissors.';
+}
+
+
+function getComputerChoice() {
     const array = ['rock', 'paper', 'scissors'];
 
-    return array[Math.floor(Math.random()*array.length)];
+    return array[Math.floor(Math.random() * array.length)];
 
 }
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
 
-    if(playerSelection != 'paper' && playerSelection != 'scissors' && playerSelection != 'rock'){
-        return "invalid Typo! Please Enter exactly, rock or paper or scissors..";
+
+function playRound(buttonClicked) {
+
+    if(buttonClicked === 'reset'){
+        reset();
+    }else{
+
+    
+
+   let playerSelection = buttonClicked.toLowerCase();
+   let  computerSelection = getComputerChoice().toLowerCase();
+   console.log('computer selected : '+computerSelection);
+   console.log('player selected : ' + playerSelection);
+   console.log('-----------------------------------------');
+    if (playerScore === 5 || computerScore === 5) {
+        return;
+    } else {
+        if (playerSelection != 'paper' && playerSelection != 'scissors' && playerSelection != 'rock') {
+            answer = "invalid Typo! Please Enter exactly, rock or paper or scissors..";
+        }
+        else if (playerSelection === computerSelection) {
+            answer = "Tie! Try again.";
+        }
+        else if (
+            (playerSelection === "rock" && computerSelection !== "paper")
+            || (playerSelection === "paper" && computerSelection !== "scissors")
+            || (playerSelection === "scissors" && computerSelection !== "rock")
+        ) {
+            answer = `You Win! '${playerSelection}' beats '${computerSelection}'`;
+            playerScore += 1;
+        }
+        else {
+            answer = `You Lose! '${computerSelection}' beats '${playerSelection}'`;
+            computerScore += 1;
+        }
     }
-    else if(playerSelection === computerSelection){
-        return "Tie! Try again.";
-    }
-    else if(playerSelection === "rock" && computerSelection !== "paper"){
-        return `You Win! '${playerSelection}' beats '${computerSelection}'`;
-    }
-    else if(playerSelection === "paper" && computerSelection !== "scissors"){
-        return `You Win! '${playerSelection}' beats '${computerSelection}'`;
-    }
-    else if(playerSelection === "scissors" && computerSelection !== "rock"){
-        return `You Win! '${playerSelection}' beats '${computerSelection}'`;
+}
+
+    if(playerScore === 5){
+        resultText.textContent = "You won the Match!";
+    }else if(computerScore === 5){
+        resultText.textContent = "You Lose the Match!";
     }
     else{
-        return `You Lose! '${computerSelection}' beats '${playerSelection}'`;
+        resultText.textContent = answer;
     }
+
+    playerTotal.textContent = playerScore;
+    computerTotal.textContent = computerScore;
+
 }
 
 
-const playerSelection = prompt("Let's play Rock Paper Scissors!")
+
+
+
+
+// const playerSelection = prompt("Let's play Rock Paper Scissors!")
+
 const computerSelection = getComputerChoice();
+// console.log(computerSelection);
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        let playerSelection = button.textContent;
+        playRound(playerSelection);
+    })
+})
 
 
-alert(playRound(playerSelection, computerSelection));
+// alert(playRound(playerSelection, computerSelection));
 
-// console.log('Player selected : ' + playerSelection);
-// console.log('Computer selected : ' + computerSelection);
+
 
 
 
 
 // console.log(getComputerChoice());
+
+
+
+
